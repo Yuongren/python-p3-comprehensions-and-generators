689206ef4cca05e2207e0e7e2b4771cd6a345cbf
gitYoungren <youngrengitonga@gmail.com>
2024-12-02 07:47:34 +0300

    completed

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    return [num for num in num_list if num % 2 == 0]
+num_list = [12, 4, 3, 5, 7, 6, 8, 9]
+result = return_evens(num_list)
+print(result)
 
 def make_exclamation(sentence_list):
-    pass
+    return [sentence + "!" for sentence in sentence_list]
+sentence_list = ["Come", "How are you"]
+result = make_exclamation(sentence_list)
+print(result)
